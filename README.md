ec6a2cdcc84d50c2ba1cb6954ed94b829b644ff9
Gardner Vickers <gardner@vickers.me>
2019-10-22 04:56:14 -0700

    formatting

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # simulation
 
-## Simulation
-
 The goal of Simulation is to provide a set of low level components which can be
 used to write applications amenable to [FoundationDB style simulation testing](https://apple.github.io/foundationdb/testing.html).
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,4 @@
 #![allow(dead_code)]
-//! # Simulation
-//! 
 //! The goal of Simulation is to provide a set of low level components which can be
 //! used to write applications amenable to [FoundationDB style simulation testing](https://apple.github.io/foundationdb/testing.html).
 //! 
